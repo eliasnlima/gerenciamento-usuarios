04ca5d7d3de50e53ad53b7203d5c67852fca6fee
elias <eliasnerylima018@gmail.com>
2025-02-28 15:14:56 -0300

    projeto

---
+++ README.md
@@ -0,0 +1,35 @@
+# Projeto de Gerenciamento de Usuários
+
+Este projeto é uma aplicação web simples para gerenciar usuários, permitindo adicionar usuários com nome e e-mail, visualizar uma lista de todos os usuários cadastrados, e realizar buscas dinâmicas em tempo real.
+
+## Funcionalidades
+
+- **Cadastro de Usuários**: Permite cadastrar usuários com nome e e-mail.
+- **Visualização de Usuários**: Exibe todos os usuários cadastrados.
+- **Busca Dinâmica**: Busca usuários por nome em tempo real à medida que o usuário digita.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Uso
+
+### Cadastrar um Novo Usuário
+
+1. Preencha os campos `Nome` e `E-mail` no formulário de cadastro.
+2. Clique no botão `Cadastrar` para adicionar o usuário à lista.
+
+### Visualizar Usuários
+
+- Todos os usuários cadastrados são listados automaticamente na página.
+
+### Buscar Usuários
+
+- Digite o nome do usuário na barra de busca para filtrar a lista de usuários em tempo real.
+
+## Licença
+
+Este projeto está licenciado sob a Licença MIT.
+
---
+++ script.js
@@ -0,0 +1,74 @@
+let users = []
+
+function cadastraUser() {
+
+    let nome = document.getElementById("nome").value 
+    let email = document.getElementById("email").value 
+
+    users.push({nome: nome, email: email})
+
+    document.getElementById("nome").value  =  ""
+    document.getElementById("email").value  = ""
+    showUsers()
+
+    console.log(users)
+}
+
+function showUsers() {
+
+    document.getElementById("listaUser").textContent = ""
+
+    for (i=0; i < users.length; i++) {
+
+        let itemUser = document.createElement("li")
+        let nomeUser = document.createElement("p")
+        nomeUser.classList.add("nomeUser")
+        nomeUser.textContent = users[i].nome
+        let emailUser = document.createElement("p")
+        emailUser.textContent = "E-mail: " + users[i].email 
+
+        itemUser.appendChild(nomeUser)
+        itemUser.appendChild(emailUser)
+        document.getElementById("listaUser").appendChild(itemUser)
+       
+
+    }
+}
+
+function search() {
+
+    for (i=0; i < users.length; i++) {
+
+        document.getElementById("listaUser").textContent = ""
+        let inputSearch = document.getElementById("search").value
+        let searchUser = users.filter(user => user.nome.toLowerCase().includes(inputSearch.toLowerCase()))
+       
+
+    searchUser.forEach(i => {
+    
+    let itemUser = document.createElement("li")
+    let nomeUser = document.createElement("p")
+    nomeUser.classList.add("nomeUser")
+    nomeUser.textContent = i.nome
+    let emailUser = document.createElement("p")
+    emailUser.textContent = "E-mail: " + i.email
+
+    itemUser.appendChild(nomeUser)
+    itemUser.appendChild(emailUser)
+    document.getElementById("listaUser").appendChild(itemUser)
+        });
+
+    }
+
+}
+
+function limpo() {
+
+    let campo = document.getElementById("search").value 
+
+    if (campo === "") {
+        showUsers()
+    } else {
+        search()
+    }
+}
